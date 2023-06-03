82603cf761c6a16812b24987ccd92550bf76c108
GCJung <gcjung1227@gmail.com>
2023-06-03 23:16:50 +0900

    최초 실행시 구글, 게스트 로그인 선택 구현

--- Assets/Scripts/Panel/StartPanel.cs
+++ Assets/Scripts/Panel/StartPanel.cs
@@ -2,23 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase.Auth;
 
 public class StartPanel : PanelBase
 {
     [Header("Button")]
     [SerializeField] private Button button1;    //스타트 버튼
-    private new void Awake()
-    {
-        base.Awake();
 
-        button1.onClick.AddListener(() => GlobalManager.Instance.SceneLoadManager.LoadSceneAsync("Main"));
-    }
+    [Header("Login관련")]
+    [SerializeField] private Transform loginPanel;
+    [SerializeField] private Button googleLoginButton;
+    [SerializeField] private Button guestLoginButton;
+    //private new void Awake()
+    //{
+    //    base.Awake();
+    //    auth = FirebaseAuth.DefaultInstance;
+    //    loginPanel = transform.Find("LoginPanel");
+    //    button1.onClick.AddListener(() => GlobalManager.Instance.SceneLoadManager.LoadSceneAsync("Main"));
+    //}
 
     public override void InitPanel()
     {
-        Debug.Log("스타트 패널 생성");    
+        Debug.Log("스타트 패널 생성");
+        button1.onClick.AddListener(() => GlobalManager.Instance.SceneLoadManager.LoadSceneAsync("Main"));
+
+        if (!FirebaseAuthManager.Instance.isCurrentUserLoggedin())
+            ShowLoginPanel();
     }
 
+    void ShowLoginPanel()
+    {
+        loginPanel.gameObject.SetActive(true);
+        googleLoginButton.onClick.AddListener(() => FirebaseAuthManager.Instance.SignInFirebaseWithGoogleAccount(CloseLoginPanel));
+        guestLoginButton.onClick.AddListener(() => FirebaseAuthManager.Instance.SigninFirebaseWithAnonymous(CloseLoginPanel));
+    }
+    public void CloseLoginPanel()
+    {
+        loginPanel.gameObject.SetActive(false);
+    }
+    
     public override void OpenPanel()
     {
 
@@ -30,6 +52,8 @@ public class StartPanel : PanelBase
     }
     private void OnDestroy()
     {
+        googleLoginButton.onClick.RemoveAllListeners();
+        guestLoginButton.onClick.RemoveAllListeners();
         button1.onClick.RemoveAllListeners();
     }
 }
