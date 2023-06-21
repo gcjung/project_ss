651bc9660391e9895e5cb2eb4300fdd4b49a76fa
YeongJongLee <dldudwhd723@gmail.com>
2023-06-21 20:35:55 +0900

    스탯 슬롯 생성 구현 (버튼 기능은 미구현)

---
+++ Assets/Scripts/UI/StatusSlotScroll.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class StatusSlotScroll : MonoBehaviour
+{
+    [Header("Create Slots")]
+    [SerializeField] private GameObject statusSlot;
+    [SerializeField] private Transform content;
+
+    private List<StatusSlot> slots = new List<StatusSlot>();
+
+    public class StatusSlot
+    {
+        public int statusLevel;
+        public string statusName;
+        public double statusValue;
+        public double cost;
+        public event Action buttonClick;
+        public StatusSlot(int statusLevel, string statusName, double statusValue, double cost, Action buttonClick)
+        {// statusLevel만 데이터테이블에 저장된 값으로 받아와서 계산해주면 됨
+            this.statusLevel = statusLevel;
+            this.statusName = statusName;
+            this.statusValue = statusValue;
+            this.cost = cost;
+            this.buttonClick += buttonClick;
+        }
+    }
+    private void Start()
+    {
+        StatusSlot slot1 = new StatusSlot(1, "공격력", 523, 1, () => Debug.Log("슬릇1"));
+        StatusSlot slot2 = new StatusSlot(1, "체력", 5233, 1, () => Debug.Log("슬릇2"));
+        StatusSlot slot3 = new StatusSlot(1, "공격속도", 52333, 1, () => Debug.Log("슬릇3"));
+        StatusSlot slot4 = new StatusSlot(1, "치명타 확률", 5233333, 1, () => Debug.Log("슬릇4"));
+
+        slots.Add(slot1);
+        slots.Add(slot2);
+        slots.Add(slot3);
+        slots.Add(slot4);
+
+        CreateSlots();
+    }
+
+    private void CreateSlots()
+    {
+        if (slots.Count > 0)
+        {
+            for (int i = 0; i < slots.Count; i++)
+            {
+                var slot = Instantiate(statusSlot, content);
+
+                var levelText = slot.transform.Find("StatusLevel_Text");
+                levelText.gameObject.GetComponent<TextMeshProUGUI>().text = "LV" + slots[i].statusLevel.ToString();
+
+                var nameText = slot.transform.Find("StatusName_Text");
+                nameText.gameObject.GetComponent<TextMeshProUGUI>().text = slots[i].statusName;
+
+                var valueText = slot.transform.Find("StatusValue_Text");
+                valueText.gameObject.GetComponent<TextMeshProUGUI>().text = Utill.BigNumCalculate(slots[i].statusValue);
+
+                var button = slot.transform.Find("LevelUp_Button");
+                button.gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = Utill.BigNumCalculate(slots[i].cost) + " G";
+
+                //button.gameObject.GetComponent<Button>().onClick.AddListener();
+            }
+        }
+        else
+        {
+            Debug.LogError("배열이 비어있음");
+        }
+    }
+}
