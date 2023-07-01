54a240e8edee38f845a9b3b26e36561ac67bcb80
YeongJongLee <dldudwhd723@gmail.com>
2023-07-01 21:41:18 +0900

    asd

---
+++ Assets/Scripts/Panel/SkillPannel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SkillPannel : MonoBehaviour
+{
+    [Header("Skills")]
+    [SerializeField] private Button auto;
+    [SerializeField] private GameObject skill1;
+    [SerializeField] private GameObject skill2;
+    [SerializeField] private GameObject skill3;
+    [SerializeField] private GameObject skill4;
+    [SerializeField] private GameObject skill5;
+    [SerializeField] private GameObject skill6;
+}
